af3092a2e2c896075b439d5bb199a8a5eced4616
Joshua Williams <j.o-williams@outlook.com>
2020-04-03 11:54:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   pokemon = ["bulbasaur", "chimchar", "garchomp", "deoxys"]
-  pokemon
+  pokemon.pop
 end
 
 def remove_element_from_start_of_array(array)
