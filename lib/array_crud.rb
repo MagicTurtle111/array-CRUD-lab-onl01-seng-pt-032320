2fb1fc3c819f8166e6694385630012d7bf1c87b0
Joshua Williams <j.o-williams@outlook.com>
2020-04-03 11:46:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-create_an_array = ["bulbasaur", "chimchar", "garchomp", "deoxys"]
+pokemon = ["bulbasaur", "chimchar", "garchomp", "deoxys"]
 
 end
 
