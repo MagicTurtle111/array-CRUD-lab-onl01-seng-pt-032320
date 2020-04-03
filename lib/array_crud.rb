fb9ce1eea91071762c18dac2dc395bab21f5a12c
Joshua Williams <j.o-williams@outlook.com>
2020-04-03 11:43:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 
 def add_element_to_end_of_array(array, element)
-  create_an_array = ["bulbasaur", "chimchar", "garchomp", "deoxys"].
+  create_an_array << piplup
 end
 
 def add_element_to_start_of_array(array, element)
