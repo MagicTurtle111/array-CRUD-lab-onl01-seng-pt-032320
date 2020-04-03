c91a3228a590146b8856c47352269faa67e01af2
Joshua Williams <j.o-williams@outlook.com>
2020-04-03 12:00:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  pokemon = ["bulbasaur", "chimchar",|"am", "garchomp", "deoxys"]
+  pokemon[2]
 end
 
 def retrieve_first_element_from_array(array)
