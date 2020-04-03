168f1654b292f69411f5c15cdefe75b8e4ec4207
Joshua Williams <j.o-williams@outlook.com>
2020-04-03 11:54:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  pokemon = ["bulbasaur", "chimchar", "garchomp", "deoxys"]
+  pokemon = ["bulbasaur", "chimchar", "garchomp", "deoxys","arrays!"]
   pokemon.pop
 end
 
